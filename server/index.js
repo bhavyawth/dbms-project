8a9ddfb21ee6ef37aff9b065f69320fb5d98f987
Bhavya <bbhojwani31@gmail.com>
2025-05-31 01:16:54 +0530

    working auth and fixed index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const path = require('path');
 const { connectToMongoDb } = require('./config/connection');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const authRoutes = require('./routes/authRoutes');
+//const listingRoutes = require('./routes/listingRoutes');
+
 
 connectToMongoDb('mongodb://127.0.0.1:27017/rentngo')
   .then(() => console.log('Connected to MongoDB'))
@@ -8,6 +13,20 @@ connectToMongoDb('mongodb://127.0.0.1:27017/rentngo')
 
 const app = express();
 const PORT = 3000;
+
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use("/api/auth", authRoutes);
+//app.use("/api/listing", listingRoutes);
 
 
 app.listen(PORT, () => {
